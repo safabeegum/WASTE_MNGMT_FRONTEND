e5dd107c845dd6d3ad603865e9da79320868b6e8
safabeegum <pasafabeegum@gmail.com>
2024-09-02 22:46:37 +0530

    UserSignIn Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserHome from './components/UserHome';
 
 function App() {
   return (
-    <div>
-      <UserSignUp/>
+    <BrowserRouter>
 
-    </div>
+    <Routes>
+      <Route path='/' element={<UserSignIn/>}/>
+      <Route path='/usersignup' element={<UserSignUp/>}/>
+      <Route path='/userhome' element={<UserHome/>}/>
+
+    </Routes>
+    </BrowserRouter>
   );
 }
 
