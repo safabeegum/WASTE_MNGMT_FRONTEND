6327df923d8d0beabbfd97b7101945680d2c4e93
safabeegum <pasafabeegum@gmail.com>
2024-09-04 06:37:50 +0530

    UserHome Completed

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -2,7 +2,59 @@ import React from 'react'
 
 const UserHome = () => {
   return (
-    <div>UserHome</div>
+    <div>
+        <div className="container">
+        <div className="row g-4">
+      <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          <div class="card border-light">
+                  <img height="300px"  src="https://img.freepik.com/free-vector/recycle-colorful-geometric-gradient-logo-vector_343694-1249.jpg?t=st=1725411534~exp=1725415134~hmac=aacf4324d69d37686b39e2446f6f7ab49cfd25e1072955de96d86af9a4e0ee33&w=740" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                  <h5 class="card-title text-center">WASTE PICKUP REQUEST</h5>
+                  <div class="text-center">
+                  <button type="button" class="btn btn-secondary">KNOW MORE</button>
+                  </div>
+                  </div>
+                  </div>
+          </div>
+
+          <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          <div class="card border-light">
+                  <img height="300px"  src="https://img.freepik.com/free-vector/recycle-colorful-geometric-gradient-logo-vector_343694-1249.jpg?t=st=1725411534~exp=1725415134~hmac=aacf4324d69d37686b39e2446f6f7ab49cfd25e1072955de96d86af9a4e0ee33&w=740" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                  <h5 class="card-title text-center">PAYMENT</h5>
+                  <div class="text-center">
+                  <button type="button" class="btn btn-secondary">KNOW MORE</button>
+                  </div>
+                  </div>
+                  </div>
+          </div>
+
+          <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          <div class="card border-light">
+                  <img height="300px"  src="https://img.freepik.com/free-vector/recycle-colorful-geometric-gradient-logo-vector_343694-1249.jpg?t=st=1725411534~exp=1725415134~hmac=aacf4324d69d37686b39e2446f6f7ab49cfd25e1072955de96d86af9a4e0ee33&w=740" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                  <h5 class="card-title text-center">RECIEPT</h5>
+                  <div class="text-center">
+                  <button type="button" class="btn btn-secondary">KNOW MORE</button>
+                  </div>
+                  </div>
+                  </div>
+          </div>
+
+          <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          <div class="card border-light">
+                  <img height="300px"  src="https://img.freepik.com/free-vector/recycle-colorful-geometric-gradient-logo-vector_343694-1249.jpg?t=st=1725411534~exp=1725415134~hmac=aacf4324d69d37686b39e2446f6f7ab49cfd25e1072955de96d86af9a4e0ee33&w=740" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                  <h5 class="card-title text-center">FEEDBACK</h5>
+                  <div class="text-center">
+                  <button type="button" class="btn btn-secondary">KNOW MORE</button>
+                  </div>
+                  </div>
+                  </div>
+          </div>
+    </div>
+    </div>
+    </div>
   )
 }
 
