a4b69658a373f014e052cd78e0722578eab2804f
safabeegum <pasafabeegum@gmail.com>
2024-09-04 15:29:34 +0530

    CollectHome Completed

---
+++ src/components/CollectHome.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const CollectHome = () => {
+  return (
+    <div>
+        <div className="container">
+        <div className="row g-4">
+      <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          <div class="card border-light">
+                  <img height="300px"  src="https://img.freepik.com/free-vector/recycle-colorful-geometric-gradient-logo-vector_343694-1249.jpg?t=st=1725411534~exp=1725415134~hmac=aacf4324d69d37686b39e2446f6f7ab49cfd25e1072955de96d86af9a4e0ee33&w=740" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                  <h5 class="card-title text-center">VIEW TASKS</h5>
+                  <div class="text-center">
+                  <a href="/" className="btn btn-secondary">CLICK</a>
+                  </div>
+                  </div>
+                  </div>
+          </div>
+
+          <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          <div class="card border-light">
+                  <img height="300px"  src="https://img.freepik.com/free-vector/recycle-colorful-geometric-gradient-logo-vector_343694-1249.jpg?t=st=1725411534~exp=1725415134~hmac=aacf4324d69d37686b39e2446f6f7ab49cfd25e1072955de96d86af9a4e0ee33&w=740" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                  <h5 class="card-title text-center">PICKUP CONFIRM</h5>
+                  <div class="text-center">
+                  <a href="/" className="btn btn-secondary">CLICK</a>
+                  </div>
+                  </div>
+                  </div>
+          </div>
+
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default CollectHome
